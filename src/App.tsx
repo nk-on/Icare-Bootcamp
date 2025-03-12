b16b87a0834fce39b85ef8e93f31da0db542c911
nk-on <2nkoz@proton.me>
2025-03-11 22:44:29 -0700

    Creating first react component at icare bootcamp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
-
+import Hello from './Hello'
 function App() {
 
   return (
     <>
+     <Hello />
+     <p>This is the main App component.</p>
     </>
   )
 }
---
+++ src/Hello.tsx
@@ -0,0 +1,8 @@
+function Hello(){
+    return (
+        <div>
+            <h2>Hello from React Component!</h2>
+        </div>
+    )
+};
+export default Hello;
