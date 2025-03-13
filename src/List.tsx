42fc979c7fca96a86440ba41dae9353a8436074c
nk-on <2nkoz@proton.me>
2025-03-12 22:57:02 -0700

    adding feature which sets background on red when clicking

--- src/List.tsx
+++ src/List.tsx
@@ -1,7 +1,20 @@
-function List({ items }:{items:string[]}) {
+import { useState } from "react";
+
+function List({ items }: { items: string[] }) {
+  const [clickedIdx, setClickedIdx] = useState(-1);
   return (
     <>
-      <ul>{items.map((item,idx) => <li key={idx}>{item}</li>)}</ul>
+      <ul>
+        {items.map((item, idx) => (
+          <li
+            key={idx}
+            onClick={() => setClickedIdx(idx)}
+            style={{ background: `${clickedIdx === idx ? "red" : ""}` }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
